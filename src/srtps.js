548be94b025b6193ba66f7de770cce9a833295ff
Stephen Last <stephen.last@gmail.com>
2016-09-02 11:57:52 +0100

    update

--- src/srtps.js
+++ src/srtps.js
@@ -4,32 +4,19 @@ import dom from 'xmldom'
 /**
  * the maths bit (https://github.com/mailzwj/sstp)
  */
-function getPoints (x, y, w, h, deg) {
-  var c = { x: x + w / 2, y: y + h / 2 }
-  var points = []
-  var r = Math.sqrt(Math.pow(w, 2) + Math.pow(h, 2)) / 2
-  deg = deg * Math.PI / 180
-  var deg1 = (Math.PI - Math.acos((w / 2) / r)) - parseFloat(deg)
-  var deg2 = Math.acos((w / 2) / r) - parseFloat(deg)
-  var deg3 = -Math.acos((w / 2) / r) - parseFloat(deg)
-  var deg4 = Math.PI + Math.acos((w / 2) / r) - parseFloat(deg)
-  points.push({
-    x: c.x + r * Math.cos(deg1),
-    y: c.y - r * Math.sin(deg1)
-  })
-  points.push({
-    x: c.x + r * Math.cos(deg2),
-    y: c.y - r * Math.sin(deg2)
-  })
-  points.push({
-    x: c.x + r * Math.cos(deg3),
-    y: c.y - r * Math.sin(deg3)
-  })
-  points.push({
-    x: c.x + r * Math.cos(deg4),
-    y: c.y - r * Math.sin(deg4)
-  })
-  return points
+function getPoints (x, y, w, h) {
+  const c = { x: x + w / 2, y: y + h / 2 }
+  const r = Math.sqrt(Math.pow(w, 2) + Math.pow(h, 2)) / 2
+  const deg1 = Math.PI - Math.acos((w / 2) / r)
+  const deg2 = Math.acos((w / 2) / r)
+  const deg3 = -Math.acos((w / 2) / r)
+  const deg4 = Math.PI + Math.acos((w / 2) / r)
+  return [
+    { x: c.x + r * Math.cos(deg1), y: c.y - r * Math.sin(deg1) },
+    { x: c.x + r * Math.cos(deg2), y: c.y - r * Math.sin(deg2) },
+    { x: c.x + r * Math.cos(deg3), y: c.y - r * Math.sin(deg3) },
+    { x: c.x + r * Math.cos(deg4), y: c.y - r * Math.sin(deg4) }
+  ]
 }
 
 /**
@@ -57,11 +44,11 @@ function getRectPaths (rects) {
   if (rects.length < 1) { return }
   const nodes = getRectNodeValues(rects)
   const paths = nodes.map(node => {
-    const points = getPoints(node.x, node.y, node.w, node.h, 0)
-    const moveTo = `M ${ points[0].x.toFixed(3) } ${ points[0].y.toFixed(3) } `
-    const lineTo1 = `L ${ points[1].x.toFixed(3) } ${ points[1].y.toFixed(3) } `
-    const lineTo2 = `L ${ points[2].x.toFixed(3) } ${ points[2].y.toFixed(3) } `
-    const lineTo3 = `L ${ points[3].x.toFixed(3) } ${ points[3].y.toFixed(3) } `
+    const points = getPoints(node.x, node.y, node.w, node.h)
+    const moveTo = `M ${ points[0].x.toFixed(3) } ${ points[0].y.toFixed(3) }`
+    const lineTo1 = `L ${ points[1].x.toFixed(3) } ${ points[1].y.toFixed(3) }`
+    const lineTo2 = `L ${ points[2].x.toFixed(3) } ${ points[2].y.toFixed(3) }`
+    const lineTo3 = `L ${ points[3].x.toFixed(3) } ${ points[3].y.toFixed(3) }`
     return `${moveTo} ${lineTo1} ${lineTo2} ${lineTo3}`
   })
   return `${ paths.join('') } Z`
